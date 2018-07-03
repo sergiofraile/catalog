866c435d868a64833e4dca6bf61fe6c658f11abc
Sergio Fraile <s.frailec@gmail.com>
2018-07-03 19:02:04 +0100

    Initial Commit

---
+++ Catalog/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Catalog
+//
+//  Created by Sergio Fraile Carmena on 03/07/2018.
+//  Copyright © 2018 Sergio Fraile. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
